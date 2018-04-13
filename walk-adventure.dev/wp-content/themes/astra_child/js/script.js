e40306553e23d41afb21be3c05fd98342017ce07
aguirre-cordova <aguirre.cordova@me.com>
2018-04-13 13:20:55 +0300

    Create README.md

--- walk-adventure.dev/wp-content/themes/astra_child/js/script.js
+++ walk-adventure.dev/wp-content/themes/astra_child/js/script.js
@@ -1,3 +1,9 @@
 //
-jQuery(document).ready(function() {alert("Hello")});
+//jQuery(document).ready(function() {alert("Hello")});
+
+jQuery(document).ready(function(){
+  jQuery("#submit").click(function() {
+    alert("Hello1");
+  });
+});
 //jQuery('body').append('<a href="http://www.google.com">Перейти в Google</a>');
